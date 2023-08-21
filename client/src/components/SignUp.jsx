8255893c83f421bda3c37549f627e720429e4038
joshuaobare <jobare.obare@gmail.com>
2023-08-21 13:37:40 +0300

    added formData state to SignUp component and onChange handler function

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 /* eslint-disable react/no-unescaped-entities */
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSignUpChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
   return (
     <div className="sign-up">
       <div>
@@ -9,26 +26,50 @@ export default function SignUp() {
       <form action="">
         <div>
           <label htmlFor="fullName">Full Name</label>
-          <input type="text" name="fullName" id="fullName" />
+          <input
+            type="text"
+            name="fullName"
+            id="fullName"
+            value={formData.fullName}
+            onChange={handleSignUpChange}
+          />
         </div>
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={formData.username}
+            onChange={handleSignUpChange}
+          />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" value={formData.email} />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formData.password}
+            onChange={handleSignUpChange}
+          />
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password</label>
-          <input type="password" name="confirmPassword" id="confirmPassword" />
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleSignUpChange}
+          />
         </div>
         <div>
-            <button>Sign Up</button>
+          <button>Sign Up</button>
         </div>
       </form>
     </div>
