947bf8fa006a2082ff471be14f6dcc365dd50229
joshuaobare <jobare.obare@gmail.com>
2023-08-21 12:34:07 +0300

    created user signup controller middleware function

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 exports.user_get = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
@@ -8,3 +10,80 @@ exports.user_get = asyncHandler(async (req, res, next) => {
 
   res.json({ user });
 });
+
+exports.users_get = asyncHandler(async (req, res, next) => {
+  const users = await User.find({}).exec();
+  res.json({ users });
+});
+
+exports.users_create = [
+  body("fullName")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Name too short"),
+
+  body("username")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Username too short")
+    .custom(async (value) => {
+      const user = await User.findOne({ where: { name: value } });
+      if (user) {
+        throw new Error("Username already in use!");
+      }
+      return true;
+    }),
+
+  body("email")
+    .trim()
+    .escape()
+    .isEmail()
+    .withMessage("Enter valid email address")
+    .custom(async (value) => {
+      const user = await User.findUserByEmail(value);
+      if (user) throw new Error("Email address already in use!");
+      return true;
+    }),
+
+  body("password")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Password too short"),
+
+  body("confirmPassword")
+    .trim()
+    .escape()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) throw new Error("Passwords must match!");
+      return true;
+    }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const { fullName, password, username, email } = req.body;
+
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const user = new User({
+          username,
+          fullName,
+          password: hashedPassword,
+          email,
+        });
+
+        if (!errors.isEmpty()) {
+          res.json({ errors: errors.array() });
+          return;
+        } else {
+          await user.save();
+          res.json({ message: "User created successfully" });
+        }
+      }
+    });
+  }),
+];
