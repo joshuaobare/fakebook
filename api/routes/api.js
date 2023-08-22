c65c2be78113b8d7f11206d62a059aa409cee127
joshuaobare <jobare.obare@gmail.com>
2023-08-22 10:04:16 +0300

    added passport authentication to api routes

--- api/routes/api.js
+++ api/routes/api.js
@@ -1,16 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController")
+const userController = require("../controllers/userController");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 // USER ROUTES
-router.get("/users", userController.users_get)
+router.get(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  userController.users_get
+);
 
-router.get("/user/:id", userController.user_get)
+router.get(
+  "/user/:id",
+  passport.authenticate("jwt", { session: false }),
+  userController.user_get
+);
 
-router.post("/user", userController.user_create)
+router.post("/user", userController.user_create);
 
-router.post("login", userController.login_user)
+router.post("/login", userController.login_user);
 
-router.post("logout", userController.logout_user)
+router.post("/logout", userController.logout_user);
 
-module.exports = router
+module.exports = router;
