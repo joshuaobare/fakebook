0b17a8f32bc7f908a890ab63298b785f67f57fcd
joshuaobare <jobare.obare@gmail.com>
2023-08-22 18:07:58 +0300

    created function to populate users

--- api/seeds.js
+++ api/seeds.js
@@ -1,5 +1,27 @@
 const User = require("./models/userModel");
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.KEY;
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
-console.log(faker.person.fullName())
+async function createUser() {
+  const user = new User({
+    username: faker.internet.userName(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    password: process.env.TEST_PASS,
+    joinedAt: faker.date.past(),
+  });
+
+  await user.save();
+}
+
+for (let x = 0; x < 10; x++) {
+  createUser();
+}
