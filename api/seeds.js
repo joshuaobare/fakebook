3148e056afe3b862bb67eb81d7fd912c18de4871
joshuaobare <jobare.obare@gmail.com>
2023-08-22 19:12:55 +0300

    created function to createPosts within seeds.js

--- api/seeds.js
+++ api/seeds.js
@@ -1,4 +1,5 @@
 const User = require("./models/userModel");
+const Post = require("./models/postModel")
 const { faker } = require("@faker-js/faker");
 require("dotenv").config();
 const mongoose = require("mongoose");
@@ -21,7 +22,36 @@ async function createUser() {
 
   await user.save();
 }
-
+/*
 for (let x = 0; x < 10; x++) {
   createUser();
+} */
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+async function createPosts() {
+    const users = await User.find({}).exec()
+    
+    users.forEach(async (user) => {
+        for (let x = 0; x < (randomIntFromInterval(2,10)); x++) {
+            const randomSlice = users.slice(randomIntFromInterval(0,5),7)
+            const userIds = []
+            randomSlice.forEach(user => {
+                userIds.push(user._id)
+            })
+            const post = new Post({
+                userId: user._id,
+                likes: [...userIds],
+                text: faker.lorem.paragraphs(),
+                timestamp: faker.date.past()
+            })
+
+            await post.save()         
+
+          } 
+    })
+    
 }
+
