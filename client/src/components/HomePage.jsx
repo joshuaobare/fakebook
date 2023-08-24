8b0e41f671709335f08330e855f1dfd93a03af0f
joshuaobare <jobare.obare@gmail.com>
2023-08-24 20:57:54 +0300

    updated styling

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,7 +1,12 @@
+import fblogo from "../assets/fblogo.png";
+
 const HomePage = () => {
     return(
-        <div>
-            
+        <div className="homepage">
+            <div className="homepage-create-post">
+            <img src={fblogo} alt="Profile Pic Icon" className="navbar-profile-pic" />
+                <input type="text" placeholder="What's on your mind, user?" className="homepage-create-post-input" />
+            </div>
         </div>
     )
 };
