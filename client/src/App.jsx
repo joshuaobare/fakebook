f32de5de5c677bc13f2f5cf1b4096a93e1869520
joshuaobare <jobare.obare@gmail.com>
2023-08-24 18:59:37 +0300

    populated NavBar component, added Material Icon link to index.html and addded images to assets folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
 import HomePage from './components/HomePage'
+import NavBar  from './components/NavBar'
 
 function App() {
   
 
   return (
     <>      
+      <NavBar  />
       <SignUp />
     </>
   )
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,38 @@
-const NavBar = () => {
+import fblogo from "../assets/fblogo.png";
 
-}
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={fblogo} alt="Fakebook Logo" className="navbar-logo" />
+        <div className="navbar-search-section">
+          <span className="material-symbols-outlined navbar-search-icon">
+            search
+          </span>
+          <input
+            type="text"
+            placeholder="Search Fakebook"
+            className="navbar-search-input"
+          />
+        </div>
+      </div>
+      <div className="navbar-center">
+        <div>
+          <span className="material-symbols-outlined navbar-middle-tab">
+            home
+          </span>
+        </div>
+        <div>
+          <span className="material-symbols-outlined navbar-middle-tab">
+            group
+          </span>
+        </div>
+      </div>
+      <div className="navbar-right">
+        <img src="" alt="Profile Pic Icon" />
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
