666f7da835e43fb711244b5e62be991976de03f1
joshuaobare <jobare.obare@gmail.com>
2023-08-26 14:58:37 +0300

    updated Post component layout

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -3,32 +3,41 @@ import { useEffect, useState } from "react";
 
 const Post = (props) => {
   const [user, setUser] = useState({});
-  
+
   useEffect(() => {
     const fetchUser = async () => {
-        const request = await fetch(
-          `http://localhost:3000/api/user/${props.post.userId}`,
-          {
-            method: "GET",
-            headers: {
-              "Content-type": "application/json",
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          }
-        );
-        const response = await request.json()
-        console.log(response)
-        setUser(response.user)
-      };
-      fetchUser()
+      const request = await fetch(
+        `http://localhost:3000/api/user/${props.post.userId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      const response = await request.json();
+      console.log(response);
+      setUser(response.user);
+    };
+    fetchUser();
   }, []);
 
   return (
     <div>
-      <div>{user.username}</div>
+      <div>
+        <img src={user.avatar} alt="User Avatar" className="navbar-profile-pic"/>
+        <div>
+          <div>{user.username}</div>
+          <div>{props.post.timestamp}</div>
+        </div>
+      </div>
       <div>{props.post.text}</div>
-      <div>{props.post.timestamp}</div>
-      <div>{props.post.userId}</div>
+      <div>
+        <div>Like</div>
+        <div>Comment</div>
+      </div>
+      <div></div>
     </div>
   );
 };
