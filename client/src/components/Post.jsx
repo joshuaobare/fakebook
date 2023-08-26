edeee7c579a334c9845cb67f4c3bb1ec15087ab1
joshuaobare <jobare.obare@gmail.com>
2023-08-26 14:50:35 +0300

    added function within Post to fetch userData

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,12 +1,36 @@
-const Post = (props) => {
-    return (
-        <div>
-            <div>{props.post.userId}</div>
-            <div>{props.post.text}</div>
-            <div>{props.post.timestamp}</div>
-            <div>{props.post.userId}</div>
-        </div>
-    )
-}
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 
-export default Post
+const Post = (props) => {
+  const [user, setUser] = useState({});
+  
+  useEffect(() => {
+    const fetchUser = async () => {
+        const request = await fetch(
+          `http://localhost:3000/api/user/${props.post.userId}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        const response = await request.json()
+        console.log(response)
+        setUser(response.user)
+      };
+      fetchUser()
+  }, []);
+
+  return (
+    <div>
+      <div>{user.username}</div>
+      <div>{props.post.text}</div>
+      <div>{props.post.timestamp}</div>
+      <div>{props.post.userId}</div>
+    </div>
+  );
+};
+
+export default Post;
