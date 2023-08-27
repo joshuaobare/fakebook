adebf6844c6a29cbc6c1c748a205c53ff94c45c0
joshuaobare <jobare.obare@gmail.com>
2023-08-27 12:42:45 +0300

    created Comment component

---
+++ client/src/components/Comment.jsx
@@ -0,0 +1,13 @@
+const Comment = () => {
+  return (
+    <div className="comment">
+      <img src="" alt="Commenter Avatar" />
+      <div>
+        <div>Username</div>
+        <div>Comment text</div>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
