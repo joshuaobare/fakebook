4d12968b3c4e785d44b70c2e8c52cb510d720997
joshuaobare <jobare.obare@gmail.com>
2023-08-31 20:30:37 +0300

    updated Profile component layout and added logos to the aside

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,4 +1,8 @@
 import fblogo from "../assets/fblogo.png";
+import educationlogo from "../assets/educationlogo.png";
+import locationlogo from "../assets/locationlogo.png";
+import worklogo from "../assets/worklogo.png";
+import rellogo from "../assets/rellogo.png";
 
 const Profile = () => {
   const style = {
@@ -19,7 +23,23 @@ const Profile = () => {
           </div>
         </div>
         <div className="profile-bottom">
-          <aside></aside>
+          <aside>
+            <h3>Intro</h3>
+            <div className="profile-intro-item">
+              <img src={worklogo} alt="" /> Works At
+            </div>
+            <div className="profile-intro-item">
+              <img src={educationlogo} alt="" /> Education
+            </div>
+            <div className="profile-intro-item">
+              {" "}
+              <img src={locationlogo} alt="" /> Home Location
+            </div>
+            <div className="profile-intro-item">
+              {" "}
+              <img src={rellogo} alt="" /> Relationship Status
+            </div>
+          </aside>
           <div></div>
         </div>
       </div>
