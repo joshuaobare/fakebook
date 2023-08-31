bcf64fd3412877bee641b4cc78573b4a258d9e25
joshuaobare <jobare.obare@gmail.com>
2023-08-31 20:04:26 +0300

    profile component header layout created

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,14 +1,30 @@
-const Profile = () => {
-    return (
-        <div className="profile">
-            <div></div>
-            <div>
-                <aside></aside>
-                <div></div>              
-                
-            </div>
-        </div>
-    )
-}
+import fblogo from "../assets/fblogo.png";
 
-export default Profile
+const Profile = () => {
+  const style = {
+    backgroundImage: `url(${fblogo})`,
+    background: "cover",
+  };
+  return (
+    <div className="profile">
+      <div className="profile-cont">
+        <div className="profile-top">
+          <div className="profile-cover" style={style}></div>
+          <div className="profile-header">
+            <img src={fblogo} alt="Profile Picture" className="profile-pic" />
+            <div>
+              <h1 className="profile-header-username">Profile Name</h1>
+              <div>Friend Count</div>
+            </div>
+          </div>
+        </div>
+        <div className="profile-bottom">
+          <aside></aside>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
