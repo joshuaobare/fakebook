048071e1e0bb0aa25e88fdf0f44021527f831e25
joshuaobare <jobare.obare@gmail.com>
2023-09-04 19:31:25 +0300

    created controller function to create comments

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -1,5 +1,6 @@
 const Comment = require("../models/commentModel");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.comments_get = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
@@ -8,3 +9,30 @@ exports.comments_get = asyncHandler(async (req, res, next) => {
 
   res.json({ comments });
 });
+
+exports.create_comment = [
+  body("text")
+    .escape()
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Comment cannot be blank"),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const { userId, text, postId } = req.body;
+
+    const comment = new Comment({
+      userId,
+      text,
+      postId
+    });
+
+    if (!errors.isEmpty()) {
+      res.json({ errors: errors.array() });
+      return;
+    } else {
+      await comment.save();
+      res.json({ message: "Comment created successfully" });
+    }
+  }),
+];
