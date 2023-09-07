7960285af3744450874067e08e1e055d92b49068
joshuaobare <jobare.obare@gmail.com>
2023-09-07 13:28:44 +0300

    added functions to fetch friends and friend requesters' profiles within FriendsTab component

--- client/src/components/FriendsTab.jsx
+++ client/src/components/FriendsTab.jsx
@@ -1,14 +1,63 @@
-const FriendsTab = () => {
-    return(
-        <div className="friends-tab">
-            <div>
-                <h1>Friend Requests</h1>
-            </div>
-            <div>
-                <h1>Friends</h1>
-            </div>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
 
-export default FriendsTab
+const FriendsTab = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [friendRequests, setFriendRequests] = useState([]);
+  const [friends, setFriends] = useState([]);
+  const [profiles, setProfiles] = useState([]);
+
+  const populateFriends = (profiles) => {
+    const requestProfiles = [];
+    const friendProfiles = [];
+
+    user.friendRequests.forEach((request) => {
+      const requester = profiles.find(
+        (user) => user._id.toString() === request.toString()
+      );
+      requestProfiles.push(requester);
+    });
+
+    user.friends.forEach((friend) => {
+      const foundFriend = profiles.find(
+        (user) => user._id.toString() === friend.toString()
+      );
+      friendProfiles.push(foundFriend);
+    });
+
+    setFriendRequests(requestProfiles);
+    setFriends(friendProfiles);
+  };
+
+  const fetchProfiles = async () => {
+    const request = await fetch(`http://localhost:3000/api/users`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const response = await request.json();
+
+    if (response.users !== undefined) {
+      setProfiles(response.users);
+      populateFriends(response.users);
+    }
+  };
+
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
+
+  return (
+    <div className="friends-tab">
+      <div>
+        <h1>Friend Requests</h1>
+      </div>
+      <div>
+        <h1>Friends</h1>
+      </div>
+    </div>
+  );
+};
+
+export default FriendsTab;
