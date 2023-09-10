5a56dcb9cc02a72c721f1fe01c847419068324f9
joshuaobare <jobare.obare@gmail.com>
2023-09-10 16:29:10 +0300

    updated Login page css

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -65,34 +65,34 @@ const Login = (props) => {
         <h2 className="login-left-bottom">Fakebook helps you connect and share with the people in your life</h2>
       </div>
       <div>
-        <form action="" method="post" onSubmit={handleLoginSubmission}>
+        <form action="" method="post" onSubmit={handleLoginSubmission} className="login-form">
           <div>
-            <label htmlFor="username">Username</label>
             <input
               type="text"
               name="username"
               id="username"
               value={formData.username}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="login-input"
               onChange={handleLoginChange}
+              placeholder="Username"
             />
             <span className="error-msg"></span>
           </div>
           <div>
-            <label htmlFor="password">Password</label>
             <input
               type="password"
               name="password"
               value={formData.password}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="login-input"
               id="password"
               onChange={handleLoginChange}
+              placeholder="Password"
             />
             <span className="error-msg"></span>
           </div>
 
           <div>
-            <button>Log In</button>
+            <button className="login-btn">Log In</button>
           </div>
           <button onClick={guestLogin}>Log In as guest</button>
           <div>{loginError ? "Invalid credentials, try again" : ""}</div>
