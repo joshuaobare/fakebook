bf6a2911b774595405c4eeeb1183e3f45bab3b2a
joshuaobare <jobare.obare@gmail.com>
2023-09-10 16:15:11 +0300

    added fonts and updated Login page css

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,7 +6,7 @@ const Login = (props) => {
     username: "",
     password: "",
   });
-  const [loginError, setLoginError] = useState(false)
+  const [loginError, setLoginError] = useState(false);
 
   const handleLoginChange = (e) => {
     const { name, value } = e.target;
@@ -23,18 +23,16 @@ const Login = (props) => {
         headers: { "Content-type": "application/json" },
         body: JSON.stringify(formData),
       });
-      const response = await request.json()
+      const response = await request.json();
 
-      if(response.token !== undefined){
-        localStorage.setItem("token", response.token)
-        localStorage.setItem("user", response.user)      
-        setLoginError(false)
-        props.loginHandler()
+      if (response.token !== undefined) {
+        localStorage.setItem("token", response.token);
+        localStorage.setItem("user", response.user);
+        setLoginError(false);
+        props.loginHandler();
       }
-
-      
     } catch (err) {
-        setLoginError(true)
+      setLoginError(true);
     }
   };
 
@@ -44,59 +42,62 @@ const Login = (props) => {
       const request = await fetch("http://localhost:3000/api/login/", {
         method: "POST",
         headers: { "Content-type": "application/json" },
-        body: JSON.stringify({username:"testerProfile", password:"random"}),
+        body: JSON.stringify({ username: "testerProfile", password: "random" }),
       });
-      const response = await request.json()
-      console.log(response)
+      const response = await request.json();
+      console.log(response);
 
-      if(response.token !== undefined){
-        localStorage.setItem("token", response.token)
-        localStorage.setItem("user", response.user)      
-        setLoginError(false)
-        props.loginHandler()
+      if (response.token !== undefined) {
+        localStorage.setItem("token", response.token);
+        localStorage.setItem("user", response.user);
+        setLoginError(false);
+        props.loginHandler();
       }
     } catch (err) {
-        setLoginError(true)
+      setLoginError(true);
     }
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form action="" method="post" onSubmit={handleLoginSubmission}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={formData.username}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            onChange={handleLoginChange}
-          />
-          <span className="error-msg"></span>
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            onChange={handleLoginChange}
-          />
-          <span className="error-msg"></span>
-        </div>
-        
-        <div>
+    <div className="login">
+      <div className="login-left">
+        <div className="login-left-header">fakebook</div>
+        <h2 className="login-left-bottom">Fakebook helps you connect and share with the people in your life</h2>
+      </div>
+      <div>
+        <form action="" method="post" onSubmit={handleLoginSubmission}>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={formData.username}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleLoginChange}
+            />
+            <span className="error-msg"></span>
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              onChange={handleLoginChange}
+            />
+            <span className="error-msg"></span>
+          </div>
+
+          <div>
             <button>Log In</button>
-        </div>
-        <button onClick={guestLogin}>Log In as guest</button>
-        <div>
-            {loginError? "Invalid credentials, try again": ""}
-        </div>
-      </form>
+          </div>
+          <button onClick={guestLogin}>Log In as guest</button>
+          <div>{loginError ? "Invalid credentials, try again" : ""}</div>
+        </form>
+      </div>
     </div>
   );
 };
