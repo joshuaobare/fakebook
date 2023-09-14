97735dd87043dba78b3c88a8c3996ff9465f1d33
joshuaobare <jobare.obare@gmail.com>
2023-09-14 19:12:25 +0300

    updated navbar with elements from previous navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -142,14 +142,42 @@ const NavBar = () => {
             alt="Fakebook Logo"
             className="navbar-logo h-8 mr-3"
           />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white logo-text">
-            Fakebook
-          </span>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white logo-text">Fakebook</span>
+          <div className="navbar-search-section">
+            <span className="material-symbols-outlined navbar-search-icon">
+              search
+            </span>            
+            <input
+              type="text"
+              placeholder="Search Fakebook"
+              className="navbar-search-input"
+              name="search"
+              value={searchValue}
+              onChange={searchHandler}
+            />
+            <div className="search-bar-results">
+            {foundProfiles.map((profile) => (
+              <li key={profile._id} className="search-bar-result-item">
+                <Link
+                  to={`/user/${profile._id}`}
+                  className="friends-tab-friend"
+                >
+                  <img
+                    src={profile.avatar}
+                    alt="friend-avatar"
+                    className="navbar-profile-pic"
+                  />
+                  <div>{profile.fullName}</div>
+                </Link>
+              </li>
+            ))}
+          </div>
+          </div>
         </Link>
         <div className="flex items-center md:order-2">
           <button
             type="button"
-            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            className="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
             id="user-menu-button"
             aria-expanded="false"
             data-dropdown-toggle="user-dropdown"
@@ -157,8 +185,8 @@ const NavBar = () => {
           >
             <span className="sr-only">Open user menu</span>
             <img
-              className="w-8 h-8 rounded-full"
-              src="/docs/images/people/profile-picture-3.jpg"
+              className="w-8 h-8 rounded-full navbar-profile-pic"
+              src={user.avatar}
               alt="user photo"
             />
           </button>
@@ -241,70 +269,51 @@ const NavBar = () => {
         >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="#"
+              <Link
+                to="/"
                 className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
               >
-                Home
-              </a>
+                <span className="material-symbols-outlined navbar-middle-tab">
+                  home
+                </span>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                to="/friends"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                About
-              </a>
+                <span className="material-symbols-outlined navbar-middle-tab">
+                  group
+                </span>
+              </Link>
             </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <div
-                className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-                id="navbar-search"
-              >
-                <div className="relative mt-3 md:hidden">
-                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg
-                      className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                      />
-                    </svg>
-                  </div>
-                  <input
-                    type="text"
-                    id="search-navbar"
-                    className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search..."
+
+            <div className="relative mt-3 md:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                   />
-                </div>
+                </svg>
               </div>
-            </li>
+              <input
+                type="text"
+                id="search-navbar"
+                className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search..."
+              />
+            </div>
           </ul>
         </div>
       </div>
